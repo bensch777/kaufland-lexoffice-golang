ce6e57634964a899b4bbb455bedab2c5bf380917
bensch777 <benjamin.neske@gmail.com>
2023-05-03 15:09:23 +0200

    Update main.go

--- main.go
+++ main.go
@@ -36,24 +36,24 @@ func main() {
 
     // Schreiben Sie die Ausgabe-CSV-Datei
     writer := csv.NewWriter(outputFile)
-    writer.Write([]string{"Auftraggeber", "Buchungsdatum", "Verwendungszweck", "Betrag"})
+    writer.Write([]string{"Auftraggeber", "Empfänger", "Buchungsdatum", "Verwendungszweck", "Betrag"})
     for _, record := range records {
-        if strings.Contains(record[1], "Verkaufserlös") && !strings.Contains(record[1], "Storno") {
+        if (strings.Contains(record[1], "Verkaufserlös") || strings.Contains(record[1], "Release") ) && !strings.Contains(record[1], "Storno")  {
             fmt.Println("Kaufland: "+record[3]+" "+record[8]+" "+record[10])
-            writer.Write([]string{"Kaufland", ""+record[0]+"", "KL-VERKAUFSERLÖS-"+record[3]+" - ", record[8]})
-            writer.Write([]string{"Kaufland", ""+record[0]+"", "KL-PROVISION-"+record[3]+"", "-"+record[12]})
+            writer.Write([]string{""+record[38]+" "+record[39]+"","Warenheim", ""+record[0]+"", "Bestellnummer: "+record[3]+"", record[8]})
+            //writer.Write([]string{"Kaufland", ""+record[0]+"", "KL-PROVISION-"+record[3]+"", "-"+record[12]})
          }  else if strings.Contains(record[1], "Auszahlung") || strings.Contains(record[1], "Payout"){
             fmt.Println("Auszahlung: "+record[3]+ " "+record[4]+" €")
-            writer.Write([]string{"Kaufland", ""+record[0]+"", "KL-AUSZAHLUNG", record[4]})
+            //writer.Write([]string{"Kaufland", ""+record[0]+"", "KL-AUSZAHLUNG", record[4]})
         }  else if strings.Contains(record[1], "Zusatzleistungen") {
             fmt.Println("Zusatzleistungen: "+record[3]+ " "+record[4]+" €")
-            writer.Write([]string{"Kaufland", ""+record[0]+"", "KL-SONSTIGES", record[4]})
+            //writer.Write([]string{"Kaufland", ""+record[0]+"", "KL-SONSTIGES", record[4]})
         }  else if strings.Contains(record[1], "Storno") {
             fmt.Println("Storno: "+record[3])
-            writer.Write([]string{"Kaufland", ""+record[0]+"", "KL-STORNO-"+record[3]+"", record[12]})
+            //writer.Write([]string{"Kaufland", ""+record[0]+"", "KL-STORNO-"+record[3]+"", record[12]})
         }  else if strings.Contains(record[1], "Lastschrift") {
             fmt.Println("Lastschrift : "+record[3]+ " "+record[4]+" €")
-            writer.Write([]string{"Kaufland", ""+record[0]+"", "KL-LASTSCHRIFT", record[4]})
+            //writer.Write([]string{"Kaufland", ""+record[0]+"", "KL-LASTSCHRIFT", record[4]})
         }
  
     }
