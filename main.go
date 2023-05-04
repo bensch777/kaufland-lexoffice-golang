098ba89c5b4e3d9c4221a4bd1f8fca77627b03be
bensch777 <benjamin.neske@gmail.com>
2023-05-04 08:51:42 +0200

    Added Files

--- main.go
+++ main.go
@@ -38,23 +38,11 @@ func main() {
     writer := csv.NewWriter(outputFile)
     writer.Write([]string{"Auftraggeber", "Empfänger", "Buchungsdatum", "Verwendungszweck", "Betrag"})
     for _, record := range records {
-        if (strings.Contains(record[1], "Verkaufserlös") || strings.Contains(record[1], "Release") ) && !strings.Contains(record[1], "Storno")  {
-            fmt.Println("Kaufland: "+record[3]+" "+record[8]+" "+record[10])
-            writer.Write([]string{""+record[38]+" "+record[39]+"","Warenheim", ""+record[0]+"", "Bestellnummer: "+record[3]+"", record[8]})
-            //writer.Write([]string{"Kaufland", ""+record[0]+"", "KL-PROVISION-"+record[3]+"", "-"+record[12]})
-         }  else if strings.Contains(record[1], "Auszahlung") || strings.Contains(record[1], "Payout"){
-            fmt.Println("Auszahlung: "+record[3]+ " "+record[4]+" €")
-            //writer.Write([]string{"Kaufland", ""+record[0]+"", "KL-AUSZAHLUNG", record[4]})
-        }  else if strings.Contains(record[1], "Zusatzleistungen") {
-            fmt.Println("Zusatzleistungen: "+record[3]+ " "+record[4]+" €")
-            //writer.Write([]string{"Kaufland", ""+record[0]+"", "KL-SONSTIGES", record[4]})
-        }  else if strings.Contains(record[1], "Storno") {
-            fmt.Println("Storno: "+record[3])
-            //writer.Write([]string{"Kaufland", ""+record[0]+"", "KL-STORNO-"+record[3]+"", record[12]})
-        }  else if strings.Contains(record[1], "Lastschrift") {
-            fmt.Println("Lastschrift : "+record[3]+ " "+record[4]+" €")
-            //writer.Write([]string{"Kaufland", ""+record[0]+"", "KL-LASTSCHRIFT", record[4]})
-        }
+        if (strings.Contains(record[5], "Verkaufserlös") || strings.Contains(record[1], "Release") ) && !strings.Contains(record[1], "Storno")  {
+            fmt.Println("Kaufland: "+record[2]+" "+record[6])
+            writer.Write([]string{""+record[3]+" "+record[4]+"","Kaufland", ""+record[0]+"", "Bestellnummer: "+record[2]+"", record[6]})
+            
+         } 
  
     }
     writer.Flush()
